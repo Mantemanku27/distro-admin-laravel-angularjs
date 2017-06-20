e1b77ff9b0d10ce39ba89c288c9e2cbc4f5edf8c
Mantemanku27 <mantemanku27@gmail.com>
2017-06-20 11:05:00 +0700

    'new-repository--distro-admin'

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap layanan aplikasi apapun.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Daftar layanan aplikasi apa saja.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Menjalankan Seeder Database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
